af7d4d0bc7517d5a6d9ef62939e3c4cbd084892d
Georgiy <heorhii.zasymenko@nure.ua>
2019-07-24 11:36:28 +0300

    RAM/Mult

--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -7,3 +7,34 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+
+@i
+M=1
+@R2
+M=0
+
+
+(LOOP)
+@i
+D=M
+@R0
+D=D-M
+@STOP
+D;JGT
+
+
+@R2
+D=M
+@R1
+D=D+M
+@R2
+M=D
+@i
+M=M+1
+@LOOP
+0;JMP
+
+(STOP)
+@STOP
+0;JMP
